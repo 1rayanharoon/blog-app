547484a619cabd23c898235d9d631a1b2a334220
Rayan Haroon <ahayanharoon@gmail.com>
2023-12-31 04:26:41 -0800

    Backend Modification

--- backend/databse/db.js
+++ backend/databse/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 const Connection = async (username, password) => {
-    const URL=`mongodb+srv://${username}:${password}@cluster0.irr8qij.mongodb.net/?retryWrites=true&w=majority`;
+    const URL="mongodb+srv://ibtsamsohail:mongo12345@cluster0.irr8qij.mongodb.net/?retryWrites=true&w=majority";
     try{
         await mongoose.connect(URL, { useNewUrlParser : true});
         console.log("Database Connected Successfully")
